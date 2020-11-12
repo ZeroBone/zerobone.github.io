c334e87bed22107b7c5e0352bdf7ef7e2844fce1
ZeroBone <zerobone21@gmail.com>
2020-11-12 13:42:39 +0100

    Deploy cdb12dc

--- docs/assets/js/search-worker-9.0.5.js
+++ docs/assets/js/search-worker-9.0.5.js
@@ -47,7 +47,7 @@ async function getDocuments() {
 ///////////////////////////////////////////////////////////////////////////////
 
 importScripts('/assets/js/minisearch/dist/umd/index.min.js');
-const INDEX_KEY = 'index--2020-11-06T16:00:42+01:00';
+const INDEX_KEY = 'index--2020-11-12T13:36:18+01:00';
 const storage = new StorageArea('mini-search/');
 
 const OPTIONS = {
