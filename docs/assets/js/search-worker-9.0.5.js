78a1ea285db0955fa7ac4589efb58b3bba34d917
ZeroBone <zerobone21@gmail.com>
2020-12-28 13:36:04 +0100

    Deploy eb30b1f

--- docs/assets/js/search-worker-9.0.5.js
+++ docs/assets/js/search-worker-9.0.5.js
@@ -47,7 +47,7 @@ async function getDocuments() {
 ///////////////////////////////////////////////////////////////////////////////
 
 importScripts('/assets/js/minisearch/dist/umd/index.min.js');
-const INDEX_KEY = 'index--2020-12-28T02:11:57+01:00';
+const INDEX_KEY = 'index--2020-12-28T13:35:04+01:00';
 const storage = new StorageArea('mini-search/');
 
 const OPTIONS = {
